02ce31f1a194dd02a186394de05165714b771ec4
demilv <gonzalo.cano@bootcamp-upgrade.com>
2024-05-23 17:51:42 +0200

    Modulo 2 Con expresiones regulares primera parte

--- main.js
+++ main.js
@@ -84,7 +84,13 @@ function showButton(){
 
 //4
 
+let valueName = getElementById("name")
+let valueEmail = getElementById("email")
+let valueCheck = getElementById("consent")
+valueName.addEventListener("focusout", (e) => {
+    let regEmail =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(e.target.value) ;
 
+})
 
 
 
