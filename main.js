ad3e521bfb43c15fbf17ba2a4530076de7ba902f
demilv <gonzalo.cano@bootcamp-upgrade.com>
2024-05-24 18:08:35 +0200

    Empezando popup

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ const scrollId = document.getElementById("progreso")
 const goUpButton = document.getElementById("up")
 const posPricing = document.getElementsByClassName("pricing")[0]
 
+const pType = document.getElementById("pricingSelector")
+
 //1
 
 function headeroptions(){                 
@@ -158,8 +160,46 @@ function onSubmit(){
 }
 
 //6
+//pType
+
+addEventListener("change", (e) => {
+    const valorElegido = e.target.value
+    Conversor(valorElegido)
+})
 
 
+function Conversor(type) {
+    let free = document.getElementById("valorFree")
+    let pro = document.getElementById("valorPro")
+    let premium = document.getElementById("valorPremium")
+    fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json')
+    .then((response) => response.json())
+    .then((res) => {
+        console.log(res.usd)
+        switch (pType.value){            
+            case "USD":
+                console.log("tamos aqui")
+                console.log(res.usd)
+                free.innerHTML = "$0"
+                pro.innerHTML ="$25"
+                premium.innerHTML ="$60"
+                break;
+            case "EUR":
+                free.innerHTML ="€0"
+                pro.innerHTML = `€${(25*res.usd.eur).toFixed(0)}`
+                premium.innerHTML = `€${(60*res.usd.eur).toFixed(0)}`
+                break;
+            case "GBP":
+                free.innerHTML ="G0"
+                pro.innerHTML = `G${(25*res.usd.gbp).toFixed(0)}`
+                premium.innerHTML = `G${(60*res.usd.gbp).toFixed(0)}`
+                break;
+            default:
+                console.log("wtf happened")
+        }
+    });
+
+}
 
 
 
